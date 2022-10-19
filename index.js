a833c9f321e94bcf354ad2bb1a4c68ff7cf0281c
alex stiles <alinnstiles@gmail.com>
2022-10-19 17:29:10 -0400

    commit tree b662952538c381267f941beeb677c619a19a414f

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+    let scuberGreetingForFeet; 
+
+    if(distance <= 400) {
+      return 'This one is on me!'
+    } else if (distance >400 && distance <2000) {
+      return 'That will be twenty bucks.'
+    } else if (distance >=2000 && distance <2500) {
+      return 'I will gladly take your thirty bucks.' 
+    } else if (distance >=2500) {
+    return 'No can do.'
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let ternaryCheckCity;
+
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let switchOnCharmFromTip;
+
+  if (tip === 'generous'){
+    return 'Thank you so much.'
+  } else if (tip === 'not as generous') {
+    return "Thank you."
+  } else {
+    return 'Bye.'
+  }
 }
